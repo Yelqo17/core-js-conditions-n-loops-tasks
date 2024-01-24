cae860bf5a005b28076f08852448a4488b974deb
Sirius Black <an.kozimirov@gmail.com>
2024-01-24 21:24:28 +0300

    feat: implement getMaxNumber function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -38,8 +38,14 @@ function isPositive(number) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  let result = a;
+  if (b > a) {
+    result = b;
+  } else if (c > a) {
+    result = c;
+  }
+  return result;
 }
 
 /**
